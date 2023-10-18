63c4aa2120ad2cf619417da409930f4c4cfb03f5
Carlos Pineda <cara_p@unicah.edu>
2023-10-18 12:29:32 -0600

    Finalizado parte del diseño con Vue

--- vue-project/src/components/TaskContent/TaskContent.vue
+++ vue-project/src/components/TaskContent/TaskContent.vue
@@ -9,16 +9,25 @@
         <div class="list-item-text">
           Sonreir
         </div>
-        <img src="../../assets/list-item-container.svg" alt="list-item" class="list-item-container">
+        <div class="list-item-check-box">
+          <img src="../../assets/check-icon.svg" alt="Check Icon" width="80%">
+        </div>
+
       </div>
       <div class="list-item">
-        <img src="../../assets/list-item-container.svg" alt="list-item">
+        <div class="list-item-text">
+          Sonreir
+        </div>
+        <div class="list-item-check-box">
+        </div>
       </div>
       <div class="list-item">
-        <img src="../../assets/list-item-container.svg" alt="list-item">
-      </div>
-      <div class="list-item">
-        <img src="../../assets/list-item-container.svg" alt="list-item">
+        <div class="list-item-text">
+          Sonreir
+        </div>
+        <div class="list-item-check-box">
+        </div>
+
       </div>
 
     </div>
@@ -55,17 +64,63 @@ export default class TaskContent extends Vue {
     display flex
     flex-direction column
     font-weight normal
+  .list-container .list-item:not(:last-child)
+    margin-bottom 40px
   .list-item
+    //display flex
+    //width 100%
+    //margin-bottom 10px
+    //background-color #42b983
+    //&-text
+    //  padding-left 55px
+    //  color white
+    //  position absolute
+    //  height 127px
+    //  display flex
+    //  align-items center
+    //&-icon
+    //  width 68%
+    //  background-color #42b983
+    //  position absolute
+    //  display flex
+    //  justify-content right
+    //&-container
+    //  width 100%
+    width 100%
+    height 100px
+    border-radius 23px
+    background-color #3A68B2
+    position relative
+    border solid 3px #E3E4E3
     display flex
+    justify-content space-between
+    padding-right 20px
+    padding-left 20px
+    align-items center
     &-text
-      padding-left 55px
       color white
-      position absolute
-      height 127px
       display flex
+      z-index 1
+      height 100%
       align-items center
-
-
-
-
+    &-check-box
+      height 85px
+      width 85px
+      background-color #1592E6
+      z-index 1
+      border-radius 8px
+      display flex
+      align-content center
+      justify-content center
+    &::after
+      content ""
+      background-color #3A68B2
+      opacity 0.46
+      position absolute
+      width 100%
+      height 100px
+      top 15px
+      left -10px
+      z-index 0
+      border-radius 23px
 </style>

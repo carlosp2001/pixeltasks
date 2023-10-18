39bbb1f1d982dfd7e84eb87e6c7a9e6c8357f121
Carlos Pineda <cara_p@unicah.edu>
2023-10-18 16:54:51 -0600

    Inicializando parte del backend con Laravel, creando modelo de tareas

---
+++ laravel-project/pixel-tasks-api/app/Http/Controllers/TaskController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //
+    public function index() {
+        return Task::all();
+    }
+
+    public function show(Task $task) {
+        // Mostrar una nota específica
+        return $task;
+    }
+
+    public function store(Request $request)
+    {
+        // Crear una nueva nota
+        $task = Task::create($request->all());
+        return $task;
+    }
+
+    public function update(Request $request, Task $task) {
+        // Actualizar una nota
+        $task->update($request->all());
+        return $task;
+    }
+
+
+    public function destroy(Task $task) {
+        // Eliminar una nota
+        $task->delete();
+        return response()->json(null, 204); // Respuesta exitosa sin contenido
+        }
+
+    public function latest() {
+        $lastTasks = Task::orderBy('created_at', 'desc')->get();
+        return $lastTasks;
+    }
+}
